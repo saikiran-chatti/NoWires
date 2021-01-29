28283eaa6769eb6f73570b10fafc56357351d05b
jayanthsaikiran <jayanthsaikiran@gmail.com>
2021-01-29 22:13:25 +0530

    Added css fixes. Need to modify the height& width of mockup

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import './Home.css'
-import Mockup from '../Mockup/Mockup'
 
 const Home = () => {
     return (
-        <div>
-            <div className="l-main">
-                <section className="home" id="home">
-                    <div className="homeContainer bd-grid">
-                        <div className="homeImg">
-                            <Mockup />
-                        </div>
-                        <div className="homeData">
-                            <p className="homeTitle">Transfer files seamlessly without hassle, Wires</p>
-                            <p className="homeDescription">Transfer files without hassle <br /> of the week.</p>
-                            <a className="homeButton" href="/home">Get Started</a>
-                        </div>
+        <div className="l-main">
+            <section className="home" id="home">
+                <div className="homeContainer bd-grid">
+                    <div className="homeImg">
+                        <img alt="mockup"
+                            src="/images/mockup@1x.svg" />
                     </div>
-                </section>
-            </div>
+                    <div className="homeData">
+                        <p className="homeTitle">Transfer files seamlessly without hassle, wires</p>
+                        <p className="homeDescription">Transfer files without hassle <br /> of the week.</p>
+                        <a className="homeButton" href="/home">Get Started</a>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
