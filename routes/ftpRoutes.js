ba059cfd149938a5f237c46560f03dc32cb0efbc
jayanthsaikiran <jayanthsaikiran@gmail.com>
2021-02-19 08:01:07 +0530

    Added icon to the website and removed the logo192 not found error

--- routes/ftpRoutes.js
+++ routes/ftpRoutes.js
@@ -12,10 +12,4 @@ router.get('/parentList', (req, res) => {
 
 })
 
-router.get('/ftptest', (req, res) => {
-    const client = new ftp('192.168.0.4', 2232, 'android', 'android')
-
-    res.send(client.sendListToFile())
-})
-
 module.exports = router;
