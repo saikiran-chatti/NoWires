8b348ff1b909b6e1f06e98a9591fb8e68ff1589b
unknown <jayanthsaikiran@gmail.com>
2021-09-16 23:12:53 +0530

    added tauri

---
+++ client/src-tauri/src/main.rs
@@ -0,0 +1,24 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri::{
+  api::process::{Command, CommandEvent},
+  Manager,
+};
+
+fn main() {
+  tauri::Builder::default()
+    .setup(|app| {
+      tauri::async_runtime::spawn(async move {
+        let (mut rx, mut child) = Command::new_sidecar("app")
+          .expect("failed to setup `app` sidecar")
+          .spawn()
+          .expect("Failed to spawn packaged node");
+      });
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("failed to run app");
+}
